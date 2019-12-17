fb2d0446ea0b27893cbba3f65956699a093c0b49
joshinils <joshua.laskowski@web.de>
2019-12-17 15:02:50 +0100

    some changes, thumbnail seems to be faster than resize

--- foo.py
+++ foo.py
@@ -16,7 +16,7 @@ if not path.exists(diffpath):
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 onlyfiles.sort()
-print('opening ', len(onlyfiles), ' images')
+print('opening', len(onlyfiles), 'images')
 images = list( zip([Image.open(mypath + i) for i in onlyfiles], onlyfiles) )
 
 # make list of lists, instead of list of tuples
@@ -25,23 +25,29 @@ for i,j in zip(images, onlyfiles):
 	i.append(False) # if it shall be saved afterward, to be set
 
 #pre-process images
+print('pre-processing images')
 w,h = images[0][0].size
-s = 4
+s = 8
 w //= s
 h //= s
 for imList in images:
 	# scale images down
- 	imList[0] = imList[0].resize( (w, h) ) 
-
-	#TODO normalize images before comparison
-	
+	#todo thumbnail?
+	imList[0].thumbnail((w,h))
+#	break
+#	imList[0] = imList[0].resize( (w, h) ) 
 
 #detect level of differences
 print('detecting differences')
+minDiff = 1000
+maxDiff = 0
+numDiff = 0
+sumDiff = 0
+avgDiff = 1 # = sumDiff / diffSum
 for index, imList in enumerate(images):
 	if index == len(images)-1:
 		break
-	print('comparing image ', index, ' of ', len(images), end=' ')
+	print('\033[Kcomparing image', index, 'of', len(images), end=' ')
 	buf1 = np.asfarray(imList[0])
 	buf2 = np.asfarray(images[index+1][0])
 
@@ -53,11 +59,18 @@ for index, imList in enumerate(images):
 			p += buf3[k][j]
 	p /= w*h
 	p = (p[0]+ p[1] + p[2]) / 3
-	print(' diff=', p, end='\r')
+	print(' diff=', "% 7.3f"%(p), end=' ')
+	minDiff = min(minDiff, p)
+	maxDiff = max(maxDiff, p)
+	numDiff += 1
+	sumDiff += p
+	avgDiff = sumDiff / numDiff
+	print("% 7.3f"%(minDiff), "% 7.3f"%(avgDiff), "% 7.3f"%(maxDiff), end='\r')
 
-	doSave = bool((p >= 6))
+	doSave = bool((p >= 3))
 	imList[2] |= doSave # save this
 	images[index+1][2] = doSave # save next
+print() # to not overwrite the last output
 
 # count different images
 diffs = 0
@@ -66,12 +79,12 @@ for imList in images:
 		diffs += 1
 
 # save images
-print('saving ', diffs, 'images')
+print('saving', diffs, 'images')
 for imList in images:
 	if imList[2]:
 		copyfile(mypath + imList[1], diffpath + imList[1])
 		#imList[0].save(diffpath + imList[1])
-print('savng done')
+print('saving done')
 
 
 
--- list.py
+++ list.py
@@ -4,6 +4,19 @@ from os import listdir
 from os.path import isfile, join
 import time
 
+
+a = range(10)
+
+print('before')
+for i in a:
+	print('\033[K', i, '************', end='\r')
+	time.sleep(.1)
+print()
+print('after1')
+print('after2')
+
+exit()
+
 a=range(1,4)
 b=range(4,7)
 c=range(7,10)
@@ -21,4 +34,5 @@ print()
 [i.append(j) for i,j in zip(L,c)]
 
 print(L)
+
 
